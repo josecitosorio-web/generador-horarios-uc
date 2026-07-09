f40c2329c3261190b9b61befe375662b8c25eadc
Jose-76825708 <76825708@continental.edu.pe>
2026-07-08 21:52:06 -0500

    Primer commit: estructura inicial del proyecto

---
+++ src/main/java/com/example/genedor_horarios/GenedorHorariosApplication.java
@@ -0,0 +1,13 @@
+package com.example.genedor_horarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenedorHorariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenedorHorariosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/genedor_horarios/GenedorHorariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.genedor_horarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenedorHorariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
