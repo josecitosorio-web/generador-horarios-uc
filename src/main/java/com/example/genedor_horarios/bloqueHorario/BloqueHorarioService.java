b9bc13c275ead20808591bb01962833b9eb5f061
Jose-76825708 <76825708@continental.edu.pe>
2026-07-09 22:38:20 -0500

    creacion de los services de cada entidad

---
+++ src/main/java/com/example/genedor_horarios/bloqueHorario/BloqueHorarioService.java
@@ -0,0 +1,5 @@
+package com.example.genedor_horarios.bloqueHorario;
+
+public interface BloqueHorarioService {
+    
+}
---
+++ src/main/java/com/example/genedor_horarios/nrc/NrcService.java
@@ -0,0 +1,5 @@
+package com.example.genedor_horarios.nrc;
+
+public interface NrcService {
+    
+}
