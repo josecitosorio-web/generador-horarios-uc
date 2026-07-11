26471a2f4ffad45f8433d6b8954d8b7766f48c2c
Jose-76825708 <76825708@continental.edu.pe>
2026-07-10 22:40:41 -0500

    Agregacion del metodo nrcElegibles: filtra por cursos y por su principalidad

--- src/main/java/com/example/genedor_horarios/nrc/NrcServiceImpl.java
+++ src/main/java/com/example/genedor_horarios/nrc/NrcServiceImpl.java
@@ -1,8 +1,25 @@
 package com.example.genedor_horarios.nrc;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 @Service
 public class NrcServiceImpl implements NrcService {
+
+    private NrcRepository nrcRepository;
+
+    public NrcServiceImpl (NrcRepository nrcRepository) {
+
+        this.nrcRepository = nrcRepository;
+
+    }
+
+    @Override
+    public List<NrcEntity> nrcElegibles (Long idCurso ,Boolean esPrincipal) {
+
+        return nrcRepository.findByCursoIdAndEsPrincipal(idCurso, esPrincipal);
+
+    }
     
 }
