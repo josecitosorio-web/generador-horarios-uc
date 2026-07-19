74d8eaa88e930d328068896381d59cf88d9db872
josecitosorio-web <josecitosorio@gmail.com>
2026-07-18 22:23:08 -0500

    integracion del crud de la entidad nrc

--- src/main/java/com/example/genedor_horarios/nrc/NrcService.java
+++ src/main/java/com/example/genedor_horarios/nrc/NrcService.java
@@ -4,6 +4,18 @@ import java.util.List;
 
 public interface NrcService {
 
+    List<Nrc> listarTodos();
+
     List<NrcEntity> nrcElegibles (Long idCurso, Boolean esPrincipal);
+
+    Nrc buscarPorCodigo (String codigo);
+
+    void registrarNrc (Long id, String codigo, Long cursoId, String docente, String modalidad, Boolean esPrincipal, String nrcVinculado);
+
+    Nrc buscarPorId (Long id);
+
+    void eliminarPorId (Long id);
+
+    
     
 }
--- src/main/java/com/example/genedor_horarios/nrc/NrcServiceImpl.java
+++ src/main/java/com/example/genedor_horarios/nrc/NrcServiceImpl.java
@@ -4,22 +4,69 @@ import java.util.List;
 
 import org.springframework.stereotype.Service;
 
+import com.example.genedor_horarios.curso.Curso;
+import com.example.genedor_horarios.curso.CursoService;
+
 @Service
 public class NrcServiceImpl implements NrcService {
 
     private NrcRepository nrcRepository;
+    private CursoService cursoService;
 
-    public NrcServiceImpl (NrcRepository nrcRepository) {
+    public NrcServiceImpl(NrcRepository nrcRepository,CursoService cursoService) {
 
         this.nrcRepository = nrcRepository;
+        this.cursoService = cursoService;
 
     }
 
     @Override
-    public List<NrcEntity> nrcElegibles (Long idCurso ,Boolean esPrincipal) {
+    public List<Nrc> listarTodos() {
+
+        return NrcAdapter.toModelList(nrcRepository.findAll());
+
+    }
+
+    @Override
+    public List<NrcEntity> nrcElegibles(Long idCurso, Boolean esPrincipal) {
 
         return nrcRepository.findByCursoIdAndEsPrincipal(idCurso, esPrincipal);
 
     }
-    
+
+    @Override
+    public Nrc buscarPorCodigo (String codigo) {
+
+        return NrcAdapter.toModel(nrcRepository.findByCodigo(codigo));
+
+    }
+
+    @Override
+    public void registrarNrc(Long id ,String codigo, Long cursoId, String docente, String modalidad, Boolean esPrincipal,
+            String nrcVinculado) {
+
+        Curso curso = cursoService.buscarPorId(cursoId);
+        Nrc nrcvinculado = buscarPorCodigo(nrcVinculado);
+
+        Nrc nrcNuevo = new Nrc(id, codigo,docente,modalidad,esPrincipal,curso,nrcvinculado);
+
+        nrcRepository.save(NrcAdapter.toEntity(nrcNuevo));
+        
+        
+    }
+
+    @Override
+    public Nrc buscarPorId (Long id) {
+
+        return NrcAdapter.toModel(nrcRepository.findById(id).orElse(null));
+
+    }
+
+    @Override
+    public void eliminarPorId (Long id) {
+
+        nrcRepository.deleteById(id);
+
+    }
+
 }
