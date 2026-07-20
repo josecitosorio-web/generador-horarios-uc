aba8f3065b5b2e48a0eb784e11dde65c5e89558b
josecitosorio-web <josecitosorio@gmail.com>
2026-07-19 22:33:17 -0500

    integracion del diseño UX para la vizualizacion de los blqoues de hroarios que registramos, que muestra tanto lo nrc indenpendientes como los nrc vinculados a otros

--- src/main/java/com/example/genedor_horarios/bloqueHorario/BloqueHorarioService.java
+++ src/main/java/com/example/genedor_horarios/bloqueHorario/BloqueHorarioService.java
@@ -2,6 +2,7 @@ package com.example.genedor_horarios.bloqueHorario;
 
 import java.time.LocalTime;
 import java.util.List;
+import java.util.Map;
 
 public interface BloqueHorarioService {
 
@@ -9,4 +10,5 @@ public interface BloqueHorarioService {
     void registrarBloque(Long id, DiaSemana dia, LocalTime horaInicio, LocalTime horaFin, String aula, String nrcCodigo);
     BloqueHorario encontrarPorId (Long id);
     void eliminarPorId(Long id);
+    Map<String,String> mostrarHorario (String nrc);
 }
