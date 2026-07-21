a5cfc3a8591feb8a93dee50c0071fefe53c95aef
josecitosorio-web <josecitosorio@gmail.com>
2026-07-20 23:20:36 -0500

    integracion del backend en la interfaz del login basico(solo pide nombre del usuario)

--- src/main/java/com/example/genedor_horarios/usuario/UsuarioController.java
+++ src/main/java/com/example/genedor_horarios/usuario/UsuarioController.java
@@ -1,12 +1,29 @@
 package com.example.genedor_horarios.usuario;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.genedor_horarios.curso.CursoService;
+import com.example.genedor_horarios.horario.HorarioService;
+
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/usuario")
 public class UsuarioController {
+
+    private final CursoService cursoService;
+    private final HorarioService horarioService;
+
+
+    public UsuarioController (CursoService cursoService, HorarioService horarioService) {
+        this.cursoService = cursoService;
+        this.horarioService = horarioService;
+    }
 
     @GetMapping("/bienvenida")
     public String bienvenida() {
@@ -14,5 +31,30 @@ public class UsuarioController {
         return "usuario/bienvenida";
 
     }
+
+    @PostMapping("/registro")
+    public String registrarUsuario(@RequestParam String nombre, HttpSession session, Model model) {
+
+        session.setAttribute("nombre", nombre );
+        
+
+        return "redirect:/usuario/cursos";
+
+    }
+
+    @GetMapping("/cursos")
+    public String mostrarCursos(HttpSession session, Model model) {
+
+        String nombre = (String) session.getAttribute("nombre");
+        
+
+        model.addAttribute("nombre", nombre);
+        model.addAttribute("cursos", cursoService.listarTodos());
+
+
+        return "usuario/curso-seleccion";
+
+    }
+
     
 }
